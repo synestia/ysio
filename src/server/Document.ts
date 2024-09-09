538bb216cae61fa889ee72213ae2dbf463799217
MainSkown <mainskown.contact@gmail.com>
2024-09-09 16:29:20 +0200

    Added onUpdate functionality

--- src/server/Document.ts
+++ src/server/Document.ts
@@ -1,30 +1,55 @@
-import * as Y from 'yjs';
-import { DocumentOptions } from './types';
-import { Namespace, Socket } from 'socket.io';
+import * as Y from "yjs";
+import { DocumentOptions } from "./types";
+import { Namespace, Socket } from "socket.io";
+
+const UPDATE_EMIT = "update-yjs";
 
 export class Document extends Y.Doc {
-    public readonly name: string
+  public readonly name: string;
 
-    private DocumentOptions?: DocumentOptions
+  private DocumentOptions?: DocumentOptions;
 
-    private documentNamespace: Namespace
+  private documentNamespace: Namespace;
 
-    constructor(name: string, documentNamespace: Namespace, documentOptions?: DocumentOptions) {
-        super()
-        this.name = name
-        this.DocumentOptions = documentOptions
-        this.documentNamespace = documentNamespace
+  constructor(
+    name: string,
+    documentNamespace: Namespace,
+    documentOptions?: DocumentOptions
+  ) {
+    super();
+    this.name = name;
+    this.DocumentOptions = documentOptions;
+    this.documentNamespace = documentNamespace;
+
+    if (this.DocumentOptions?.persistence !== undefined) {
+      // Load state from persistence
+      this.DocumentOptions.persistence.bindState(this);
+      // Emit initial state
+      this.documentNamespace.emit(UPDATE_EMIT, Y.encodeStateAsUpdate(this));
+    }
+  }
+
+  private onUpdate(update: Uint8Array, socket: Socket): void {
+    if (this.DocumentOptions?.onUpdate !== undefined) {
+      this.DocumentOptions.onUpdate(
+        update,
+        (update: Uint8Array) => {
+          Y.applyUpdateV2(this, update);
+          this.documentNamespace.emit(UPDATE_EMIT, update);
+        },
+        socket
+      );
+    } else {
+      Y.applyUpdateV2(this, update);
+      this.documentNamespace.emit(UPDATE_EMIT, update);
     }
 
-    private onUpdate(update: Uint8Array, socket?: Socket): void {
-
+    if (this.DocumentOptions?.persistence !== undefined) {
+      this.DocumentOptions.persistence.writeState(this);
     }
+  }
 
-    private awarenessUpdate(awareness: Uint8Array, socket?: Socket): void {
+  private awarenessUpdate(awareness: Uint8Array, socket?: Socket): void {}
 
-    }
-
-    private onDisconnect(socket: Socket): void {
-        
-    }
+  private onDisconnect(socket: Socket): void {}
 }
